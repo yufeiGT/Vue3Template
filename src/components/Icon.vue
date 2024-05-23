c76aca618940937f6782396ec67afbfb6e29e627
yufeiGT <769416198@qq.com>
2024-05-23 11:23:50 +0800

    Icon 添加 width 与 height 属性

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -5,31 +5,54 @@
 </template>
 
 <script lang="ts" setup>
-import { toRefs } from 'vue';
+import { computed } from 'vue';
+import { CssUnits } from '@kotron/fortress-ui';
 
-const props = defineProps({
-	icon: {
-		type: String,
-		required: true,
-	},
-	className: {
-		type: String,
-		default: '',
-	},
-	size: {
-		type: String,
-		default: '20px',
-	},
+const props = withDefaults(
+	defineProps<{
+		icon: string;
+		className?: string;
+		size?: string | number;
+		width?: string | number;
+		height?: string | number;
+	}>(),
+	{
+		size: 20,
+	}
+);
+
+const size = computed(() => toValue(props.size));
+
+const width = computed(() => {
+	if (props.width !== undefined) {
+		return toValue(props.width);
+	} else {
+		return size.value;
+	}
 });
 
-const { icon, className, size } = toRefs(props);
+const height = computed(() => {
+	if (props.height !== undefined) {
+		return toValue(props.height);
+	} else {
+		return size.value;
+	}
+});
+
+function toValue(value: string | number) {
+	if (isNaN(value as number)) {
+		return value;
+	} else {
+		return CssUnits.rem(value as number);
+	}
+}
 </script>
 
 <style lang="scss" scoped>
 .svg-icon {
 	overflow: hidden;
 	fill: currentColor;
-	width: v-bind(size);
-	height: v-bind(size);
+	width: v-bind(width);
+	height: v-bind(height);
 }
 </style>
